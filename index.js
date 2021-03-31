0924364ba294145b83ece8097d6c11fb8816b47a
Jon <jbrundagejr@gmail.com>
2021-03-31 09:59:28 -0400

    battle working, all fighter displaying, button functionality needs to be addressed

--- index.js
+++ index.js
@@ -1,26 +1,138 @@
-let header = document.querySelector("h1")
 let battleButton = document.querySelector("#battle")
-let leaderButton = document.querySelector("#leader")
-let addAFighter = document.querySelector("#addFighter")
-let fighterSpan = document.querySelector("#fighterSpan")
-let battleSpan = document.querySelector("#battleSpan")
-let fighterOneName = document.querySelector('p#fighterOneName')
-let fighterTwoName = document.querySelector('p#fighterTwoName')
-let fighterOneImage = document.querySelector('img#fighterOneAvatar')
-let fighterTwoImage = document.querySelector('img#fighterTwoAvatar')
-let allFighters = document.querySelector("#fighters")
-let displayFighter = {}
+let allFighters = document.querySelector("#allFighters")
+let leaderBoardButton = document.querySelector("#leaderboard")
+let addFighter = document.querySelector("#addFighter")
+let arena = document.querySelector("#arena")
+let fighterOneName = document.querySelector('#fighterOneName')
+let fighterOneAvatar = document.querySelector("#fighterOneAvatar")
+let fighterOneStats = document.querySelector("#fighterOneStats")
+let fighterOneWins = document.querySelector("#fighterOneWins")
+let fighterTwoName = document.querySelector('#fighterTwoName')
+let fighterTwoAvatar = document.querySelector("#fighterTwoAvatar")
+let fighterTwoStats = document.querySelector("#fighterTwoStats")
+let fighterTwoWins = document.querySelector("#fighterTwoWins")
+let displayFighter1 = {}
+let displayFighter2 = {}
 
 
-allFighters.addEventListener("click", function (){
+battleButton.addEventListener('click', function(){
     fetch("http://localhost:3000/fighters")
-    .then(res => res.json())
-    .then(function(fighterArr){
-        fighterArr.forEach(function(fighterObj){
-            turnFighterObjtoDiv(fighterObj)
+        .then(res => res.json())
+        .then(function(fightersArr){
+            console.log(fightersArr)
+            const randomFighter = fightersArr[Math.floor(
+                Math.random() * fightersArr.length
+            )]
+            console.log(randomFighter)
+            turnFighterObjToDiv(randomFighter)
+            })
+    function turnFighterObjToDiv(fighter){
+        displayFighter1 = fighter
+        displayFighter1.id = fighter.id
+        fighterOneName.innerText = fighter.name
+        fighterOneAvatar.src = fighter.image
+        fighterOneStats.innerText = (Object.entries(fighter.stats))
+        fighterOneWins.innerText = `${fighter.name} wins!`
+        console.log(fighterOneWins)
+        fighterOneWins.addEventListener('click', function(){
+            let fighterId = displayFighter1.id
+            fetch(`http://localhost:3000/fighters/${fighterId}`,{
+                method: "PATCH",
+                headers: { 
+                    "Content-Type": "application/json" 
+                },
+                body: JSON.stringify({
+                wins: displayFighter1.wins + 1,
+                }),
+            })
+                .then((res) => res.json())
+                .then(function (fighterNewWins) {
+                    displayFighter1 = fighterNewWins;
+                })
+            fetch("http://localhost:3000/fighters")
+                .then(res => res.json())
+                .then(function(fightersArr){
+                    const randomFighter = fightersArr[Math.floor(
+                        Math.random() * fightersArr.length
+                    )]
+                turnFighterObjToDiv(randomFighter)
+                })
+                function turnFighterObjToDiv(fighter){
+                    displayFighter2 = fighter
+                    displayFighter2.id = fighter.id
+                    fighterTwoName.innerText = fighter.name
+                    fighterTwoAvatar.src = fighter.image
+                    fighterTwoStats.innerText = (Object.entries(fighter.stats))
+                    fighterTwoWins.innerText = `${fighter.name} wins!`
+                }
         })
-    })
+    }
+})
+
+battleButton.addEventListener('click', function(){
+    fetch("http://localhost:3000/fighters")
+        .then(res => res.json())
+        .then(function(fightersArr){
+            console.log(fightersArr)
+            const randomFighter = fightersArr[Math.floor(
+                Math.random() * fightersArr.length
+            )]
+            console.log(randomFighter)
+            turnFighterObjToDiv(randomFighter)
+            })
+    function turnFighterObjToDiv(fighter){
+        displayFighter2 = fighter
+        displayFighter2.id = fighter.id
+        fighterTwoName.innerText = fighter.name
+        fighterTwoAvatar.src = fighter.image
+        fighterTwoStats.innerText = (Object.entries(fighter.stats))
+        fighterTwoWins.innerText = `${fighter.name} wins!`
+        fighterTwoWins.addEventListener('click', function(){
+            let fighterId = displayFighter2.id
+            fetch(`http://localhost:3000/fighters/${fighterId}`,{
+                method: "PATCH",
+                headers: { 
+                    "Content-Type": "application/json" 
+                },
+                body: JSON.stringify({
+                wins: displayFighter2.wins + 1,
+                }),
+            })
+                .then((res) => res.json())
+                .then(function (fighterNewWins) {
+                    displayFighter2 = fighterNewWins;
+                })
+            fetch("http://localhost:3000/fighters")
+                .then(res => res.json())
+                .then(function(fightersArr){
+                    const randomFighter = fightersArr[Math.floor(
+                        Math.random() * fightersArr.length
+                    )]
+                turnFighterObjToDiv(randomFighter)
+                })
+                function turnFighterObjToDiv(fighter){
+                    displayFighter1 = fighter
+                    displayFighter1.id = fighter.id
+                    fighterOneName.innerText = fighter.name
+                    fighterOneAvatar.src = fighter.image
+                    fighterOneStats.innerText = (Object.entries(fighter.stats))
+                    fighterOneWins.innerText = `${fighter.name} wins!`
+                }
+        })
+    }
+})
+
+allFighters.addEventListener('click', function(){
+    fetch("http://localhost:3000/fighters")
+        .then(res => res.json())
+        .then(function(fighters){
+            fighters.forEach(function(fighter){
+                turnFighterObjtoDiv(fighter)
+            })
+        })
+    
     function turnFighterObjtoDiv(fighter){
+        displayFighter = fighter
         let fighterDiv = document.createElement("div")
         fighterDiv.id = fighter.name
         let fighterName = document.createElement("p")
@@ -28,48 +140,6 @@ allFighters.addEventListener("click", function (){
         let fighterAvatar = document.createElement("img")
         fighterAvatar.src = fighter.image
         fighterAvatar.alt = fighter.name
-        fighterSpan.append(fighterDiv, fighterName, fighterAvatar)   
-        displayFighter = fighter
- 
+        arena.append(fighterDiv, fighterName, fighterAvatar)   
     }
 })
-function randomFighter() {
-    const index = Math.floor(Math.random() * fighterArr.length),
-        displayFighter = displayFighter[index]
-            return displayFighter.name  
-}
-
-
-function randomNumber() {
-    let randomFighter = Math.floor(Math.random() * (fighters.length))
-    console.log(randomFighter)
-    return randomFighter
-    
-}
-
-battleButton.addEventListener("click", function() {
-    let fighterId = displayFighter.id
-    console.log(fighterId)
-    fetch(`http://localhost:3000/fighters/${fighterId}`)
-        .then (res => res.json())
-        .then (function(fighter){ 
-            fighterOneName.innerText = fighter.name    
-            fighterOneImage.src = fighter.image 
-            fighterOneImage.alt = fighter.name
-            battleSpan.append(fighterOneName, fighterOneImage)   
-            displayFighter = fighter
-            
-        // fighterOneName.innerText = fighters[randomNumber()].name 
-        // fighterOneImage.src = fighters[randomNumber()][im]
-    })
-})
-
-// battleButton.addEventListener("click", function () {
-//     fetch("http://localhost:3000/fighters")
-//         .then(res => res.json())
-//         .then(randomFighter(fighters){
-//             let p = document.createElement("p")
-//             p.innerText = fighter.name
-
-           
-//     })
